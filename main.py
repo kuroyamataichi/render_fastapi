41d16e146f2aad85c5d5c260f48fa264ec385144
kuroyamataichi <23fi040@ms.dendai.ac.jp>
2025-06-19 00:30:53 +0900

    m

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ def index():
         <body>
             <h1>好きな漫画を紹介</h1>
             <h1>名探偵コナン</h1>
-            <p1>５歳から好き</p1>
             <img src="https://th.bing.com/th/id/R.71852eedc68cbb16b9b2468c40fec86b?rik=JGCVbG6QIshwmg&riu=http%3a%2f%2fwww.conan-portal.com%2fimg%2fstory_photo02.jpg&ehk=qpwXGVDU1c6hUKLRVv1JySVpwp62a%2bTerf9qIU%2bINwc%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1" alt="コナンの画像" width="400">
 
             <h1>タッチ</h1>
